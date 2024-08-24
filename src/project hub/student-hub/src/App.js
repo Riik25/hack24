fbc9c5be46f37bb702b0ab960d9c0edc04a4d2e0
Rishab Kaushik <rishabkaushik125@gmail.com>
2024-08-25 01:02:40 +0530

    React integration 2.0

--- src/project hub/student-hub/src/App.js
+++ src/project hub/student-hub/src/App.js
@@ -23,20 +23,4 @@ function App() {
 }
 
 
-import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
-}
-
 export default App;
